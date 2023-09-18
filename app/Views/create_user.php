49e827879f31f34792bfc9a51ad4d01327653680
royraflesmp <royraflesmatorangpasaribu55.com>
2023-09-19 06:22:17 +0700

    Update: css Create User

--- app/Views/create_user.php
+++ app/Views/create_user.php
@@ -4,38 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Create User</title>
+    <link rel="stylesheet" href="<?=base_url("assets/css/style.css")?>">
 <body>
-    <p>Ini Halaman Create User</p>
-    <form action="<?= base_url('/user/store') ?>" method="POST">
-        <table>
-        <tr>
-            <td>Nama</td>
-            <td>:</td>
-            <td>
-                <input type="text" name="nama" id="nama">
-            </td>
-        </tr>
-        <tr>
-            <td>NPM</td>
-            <td>:</td>
-            <td>
-                <input type="text" name="npm" id="npm">
-            </td>
-        </tr>
-        <tr>
-            <td>Kelas</td>
-            <td>:</td>
-            <td>
-                <input type="text" name="kelas" id="kelas">
-            </td>
-        </tr>
-        <tr>
-            <td colspan="3">
-                <input type="submit">
-            </td>
-    
-        </tr>
-</table>
-    </form>
+
+    <div class="box">
+        <span class="boerderLine"></span>
+        <form action="<?= base_url('/user/store') ?>" method="post">
+            <h2>Page Create User</h2>
+            <div class="inputBox">
+                <input name="nama" type="text" required="required">
+                <span>Nama</span>
+                <i></i>
+            </div>
+            <div class="inputBox">
+                <input name="kelas" type="text" required="required">
+                <span>Kelas</span>
+                <i></i>
+            </div>
+            <div class="inputBox">
+                <input name="npm" type="text" required="required">
+                <span>NPM</span>
+                <i></i>
+            </div>
+            <input type="submit" value="Kirim">
+        </form>
+    </div>
 </body>
 </html>
