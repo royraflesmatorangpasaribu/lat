f7b545d2b9214b6a83861fbd43e81136a79566bf
royraflesmp <royraflesmatorangpasaribu55.com>
2023-09-26 07:18:29 +0700

    Database Model, Validation

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -16,7 +16,7 @@ $routes->get('/profile/(:any)/(:any)/(:any)', [Home::class, 'profile']);
 
 $routes->get('/user/profile', [UserController::class, 'profile']);
 $routes->get('/user/create', [UserController::class,'create']);
-
 $routes->post('/user/store', [UserController::class,'store']);
+$routes->match(['get', 'post'],'/user/store', [UserController::class, 'store']);
 
 // $routes->get('/profile', [Home::class. 'profile']);
